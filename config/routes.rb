cb3e4af8b1d95e08fa3563ab2274c973ecb03270
當麻敬之 <taka@taimatakayukinoMacBook-Pro.local>
2017-08-19 14:12:48 +0900

    初回コミット

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new' => 'posts#new'
+
+  post 'posts/create' => 'posts#create'
+
+  get 'posts/:id' => 'posts#show'
+  get '/' => 'home#top'
+
+  get 'about' => 'home#about'
+end
