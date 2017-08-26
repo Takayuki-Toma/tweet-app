0f83b637da063762bf7411b71d64bcb0129af3f2
當麻敬之 <taka@taimatakayukinoMacBook-Pro.local>
2017-08-26 16:11:58 +0900

    編集と削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
     @posts = Post.all.order(created_at: :desc)
+  end
 
-  end
   def show
-    @id = params[:id]
+    @post = Post.find_by(id: params[:id])
   end
+
   def new
   end
+
   def create
     @post = Post.new(content: params[:content])
     @post.save
     redirect_to("/posts/index")
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])# destroyアクションの中身を作成してください
+    @post.destroy
+
+    redirect_to("/posts/index")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new' => 'posts#new'
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"# destroyアクションへのルーティングを追加してください
 
-  post 'posts/create' => 'posts#create'
 
-  get 'posts/:id' => 'posts#show'
-  get '/' => 'home#top'
-
-  get 'about' => 'home#about'
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
